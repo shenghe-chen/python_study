b7cc206eae0e775aeb5c0e016e37ffc30b407807
shenghe chen <972361669@qq.com>
2017-03-10 02:18:01 -0800

    添加判定机制

--- guess.py
+++ guess.py
@@ -1,34 +1,40 @@
-#coding=utf-8
-#猜数游戏
+#!/usr/bin/python
+# coding=utf-8
 import random
-print('游戏开始\n')
+print('\n游戏开始')
+
 
 def guess():
-	num = random.randrange(-100,100)
-	while 1:
-		input = raw_input('输入您猜的数值: ')
-		if input == "":
-			continue
-		try:
-			get = int(input)
-		except ValueError,e:
-			print('\n________请输入数字________\n')
-			continue
-		if get < num:
-			print('猜小了')
-		elif get > num:
-			print('猜大了')
-		else:
-			print('\n恭喜你，猜对了~')
-			break
+    num = random.randrange(-100, 100)
+    while 1:
+        input = raw_input('\n输入您猜的数值: ')
+        if input == "":
+            continue
+        try:
+            get = int(input)
+        except ValueError:
+            print('\n请输入数字\n')
+            continue
+        if get < num:
+            print('猜小了')
+        elif get > num:
+            print('猜大了')
+        else:
+            print('\n恭喜你，猜对了~')
+            break
+
 
 if __name__ == "__main__":
-	while True:
-		guess()
-		res = raw_input('\n是否继续游戏?[yes/no]: ')
-		if res.upper() == "YES":
-			continue
-		elif res.upper() == "NO":
-			break
-		else:
-			break
+    try:
+        guess()
+        while True:
+            res = raw_input('\n是否继续游戏?[yes/no]: ')
+            if res.upper() == "YES":
+                guess()
+            elif res.upper() == "NO":
+                break
+            else:
+                continue
+        print('\n游戏结束\n')
+    except KeyboardInterrupt:
+        print('\n游戏结束')
